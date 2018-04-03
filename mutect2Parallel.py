e71ef7fc8da868143c3f783208c16f8521b3acb4
icwells <smrupp16@gmail.com>
2018-04-03 09:04:23 -0700

    Fixed error when comparing smaple names to input bams when they included .sorted, etc.

--- mutect2Parallel.py
+++ mutect2Parallel.py
@@ -9,12 +9,18 @@ from subprocess import Popen
 from shlex import split
 from bamUtil import *
 
-def intersect(outfile, filtered):
+def intersect(outfile, cmd):
 	# Calls bcftools to get intersecting rows and summarizes output
-	
+	try:
+		bcf = Popen(split(cmd))
+		bcf.communicate()
+	except:
+		print(("\t[Error] Could not call bcftools with {}.").format(cmd))
+		return 0
 	# Initialize output csv
-	with open(outfile, "w") as output:
-		output.write("SampleA,SampleB,#PrivateA,#PrivateB,#Common,Similarity\n")
+	#with open(outfile, "w") as output:
+	#	output.write("SampleA,SampleB,#PrivateA,#PrivateB,#Common,Similarity\n")
+	return 1
 
 def filterCalls(cmd, vcf):
 	# Calls gatk to filter mutect calls
@@ -49,9 +55,12 @@ def compareVCFs(conf, outfile, vcfs):
 		if x:
 			filtered.append(x)
 	if len(filtered) == 2:
-		# Call bftools on passes and all results
 		done = 0
-		done += intersect(outfile, filtered)
+		# Call bftools on all results
+		cmd = ("bcftools isec {} {} -p {}").format(filtered[0], filtered[1])
+		done += intersect(outfile, cmd)
+		# Call bftools on passes
+		done += intersect(outfile.replace(".csv", "_PASS.csv"), cmd + " -f .,PASS")
 		if done == 2:
 			ret = True
 	return ret
@@ -92,6 +101,8 @@ def callMutect(cmd, picard, path, n, t):
 
 def submitFiles(conf, outfiles, outdir, sample):
 	# Calls MuTect2 serially over input files
+	print(sample[4])
+	quit()
 	if sample[4] < 3:
 		# Proceed if at least one combination has not been run
 		if sample[0] in outfiles.keys():
@@ -123,14 +134,14 @@ def submitFiles(conf, outfiles, outdir, sample):
 				vcfs.append(res)
 				with open(conf["log"], "a") as l:
 					l.write(("{}\tMutect\t{}\n").format(res[res.rfind("/")+1:res.rfind(".")], res))
-	'''if sample[4] == 3 or len(vcfs) == 2:
+	if sample[4] == 3 or len(vcfs) == 2:
 		# Compare output
 		print(("\n\tComparing VCFs from {}...").format(sample[0]))
-		status = compareVCFs(conf, outdir + sample[0] + ".csv" vcfs)
-		if status = True:
+		status = compareVCFs(conf, outdir + sample[0] + ".csv", vcfs)
+		if status == True:
 			# Record finished samples
 			with open(conf["log"], "a") as l:
-				l.write(("{}\tcomparison\n").format(sample[0]))'''
+				l.write(("{}\tcomparison\n").format(sample[0]))
 	if len(vcfs) < 2:
 		return ("Could not complete {}").format(sample[0])
 	else:
@@ -161,11 +172,12 @@ def getManifest(done, infile):
 					pass
 				else:
 					# [ID, Normal, A, B, status]
-					if s[2][s[2].rfind("/")+1:].replace(".bam", "") in done[s[0]]:
+					if s[2][s[2].rfind("/")+1:s[2].find(".")] in done[s[0]]:
 						a = 1
-					if s[3][s[3].rfind("/")+1:].replace(".bam", "") in done[s[0]]:
+					if s[3][s[3].rfind("/")+1:s[3].find(".")] in done[s[0]]:
 						b = 2
 			# Statuses: 0=none, 1=a_done, 2=b_done, 3=both
+			print(a, b)
 			s.append(a+b)
 			files.append(s)
 	for i in files:
@@ -277,7 +289,7 @@ def getConf(cpu, ref, infile, jar):
 def main():
 	starttime = datetime.now()
 	parser = argparse.ArgumentParser(description = "This script will call MuTect2 on a given \
-list of input files. Be sure that pysam is installed and that Samtools is in your PATH.")
+list of input files. Be sure that pysam is installed and that bcftools is in your PATH.")
 	parser.add_argument("--jar", action = "store_true", default = False,
 help = "Calls java jars indicated in the config file (Calls binaries in the environment by default).")
 	parser.add_argument("-t", type = int, default = 1,
@@ -302,8 +314,11 @@ help = "Path to config file containing reference genome, java jars (if using), a
 	pool = Pool(processes = args.t)
 	func = partial(submitFiles, conf, outfiles, args.o)
 	l = len(files)
-	# Call mutect
-	print(("\n\tCalling mutect2 with {} threads....").format(args.t))
+	t = "threads"
+	if args.t > 1:
+		t += "s"
+	# Call mutect+
+	print(("\n\tCalling mutect2 with {} {}....").format(args.t, t))
 	for x in pool.imap_unordered(func, files):
 		l -= 1
 		print(("\n\t{}. {} samples remaining").format(x, l))
